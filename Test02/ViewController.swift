4526da0fb1ccb30ac220c58850e2189661a9a63e
895686561@qq.com <895686561@qq.com>
2019-05-22 15:32:19 +0800

    btn2

--- Test02/ViewController.swift
+++ Test02/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.white;
         
-        btn = UIButton.init()
+        let btn = UIButton.init()
         self.view.addSubview(btn)
     }
 
